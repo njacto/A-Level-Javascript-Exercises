3d7ba8f47e58089ccdb4d057481b260763ad13bf
njacto <628na01@canonslade.tbft.uk>
2026-09-10 14:18:47 +0000

    Task 1.2a

---
+++ Class Exercises/1 - Introduction to Javascript/Task 2 Example - Whats the Average/script.js
@@ -0,0 +1,10 @@
+document.getElementById("Calculate Average").addEventListener("click",)
+
+function calculateAverage() {
+
+    firstNumber = parseInt(document.getElementById("number1"))
+    secondNumber = parseInt(document.getElementById("number2"))
+    thirdNumber = parseInt(document.getElementById("number3"));
+
+
+}
--- Class Exercises/1 - Introduction to Javascript/Task 2a - Mark Percentage/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 2a - Mark Percentage/script.js
@@ -6,4 +6,11 @@ function calculatePercentage() {
     // 1. Get the maximum mark and student's mark from the input fields
     // 2. Calculate the percentage using the formula: (student's mark ÷ maximum mark) × 100
     // 3. Display the result in the percentage span element
+
+    let maxMarkInput = parseInt(document.getElementById("maxMark").value)
+    let studentsMarkInput = parseInt(document.getElementById("studentMark").value);
+
+    let percentageMark = (studentsMarkInput / maxMarkInput) * 100
+
+    document.getElementById("percentage").textContent = percentageMark.toFixed(1)
 }
