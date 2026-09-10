5504f97317ece451107e75cf7eb70511e6f7e7c8
njacto <628na01@canonslade.tbft.uk>
2026-09-10 14:26:12 +0000

    Task 1.2b

--- Class Exercises/1 - Introduction to Javascript/Task 2b - Temperature Converter/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 2b - Temperature Converter/script.js
@@ -6,4 +6,16 @@ function convertTemperature() {
     // 1. Get the Celsius temperature from the input field
     // 2. Convert to Fahrenheit using the formula: F = 1.8 * C + 32
     // 3. Display the result in the fahrenheit span element
+
+    let celsiusInput = parseFloat(document.getElementById("celsius").value);
+
+    let fahreinheitValue = 1.8 * celsiusInput + 32
+    let fahrenheitOutput = document.getElementById("fahrenheit");
+
+    if (Number.isNaN(celsiusInput)) {
+        fahrenheitOutput.textContent = "Enter a number value "
+      }  else {
+        fahrenheitOutput.textContent = fahreinheitValue.toFixed(1)
+    }
+
 }
